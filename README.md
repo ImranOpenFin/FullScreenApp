bb489ac989cb328529b5d7d64961a5ec4a9f0bcc
ImranOpenFin <imran@openfin.co>
2019-05-16 14:41:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# FullScreenApp
+A Simple Full Screen Test App example
+
+-npm install && npm start
---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+
+console.log("Hello World from the Console");
+
+fin.desktop.Window.getCurrent().addEventListener("connected", ()=>{
+  
+    console.log("im connected")
+
+});
